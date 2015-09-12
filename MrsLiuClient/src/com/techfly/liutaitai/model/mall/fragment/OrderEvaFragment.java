1555e8678faaea0831bd04f4a2610a2ae5c48625
wonitani <465682538@qq.com>
2015-09-12 23:23:55 +0800

    some change and add

---
+++ MrsLiuClient/src/com/techfly/liutaitai/model/mall/fragment/OrderEvaFragment.java
@@ -0,0 +1,43 @@
+package com.techfly.liutaitai.model.mall.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.techfly.liutaitai.util.fragment.CommonFragment;
+
+public class OrderEvaFragment extends CommonFragment {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		return super.onCreateView(inflater, container, savedInstanceState);
+	}
+
+	@Override
+	public void onDestroy() {
+		// TODO Auto-generated method stub
+		super.onDestroy();
+	}
+
+	@Override
+	public void onViewCreated(View view, Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onViewCreated(view, savedInstanceState);
+	}
+
+	@Override
+	public void requestData() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
