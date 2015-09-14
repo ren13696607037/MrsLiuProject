ec251a4ba8f6b13055c32879fe287c4132f76431
xlling1127 <xlling1127@yeah.net>
2015-09-14 18:01:07 +0800

    update some change

--- MrsLiuClient/src/com/techfly/liutaitai/model/order/fragment/ServiceDetailFragment.java
+++ MrsLiuClient/src/com/techfly/liutaitai/model/order/fragment/ServiceDetailFragment.java
@@ -2,6 +2,7 @@ package com.techfly.liutaitai.model.order.fragment;
 
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.text.TextUtils;
