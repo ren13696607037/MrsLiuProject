64dbf8ef80873bac67af7bdd673a6661362a9b1e
xlling1127 <xlling1127@yeah.net>
2015-09-24 14:14:55 +0800

    update some change

--- MrsLiuClient/src/com/techfly/liutaitai/model/order/adapter/ServiceClick.java
+++ MrsLiuClient/src/com/techfly/liutaitai/model/order/adapter/ServiceClick.java
@@ -1,11 +1,13 @@
 package com.techfly.liutaitai.model.order.adapter;
 
+import android.app.Dialog;
 import android.content.Context;
 import android.view.View;
 import android.view.View.OnClickListener;
 
 import com.techfly.liutaitai.R;
 import com.techfly.liutaitai.model.mall.bean.Service;
+import com.techfly.liutaitai.util.AlertDialogUtils;
 import com.techfly.liutaitai.util.Constant;
 import com.techfly.liutaitai.util.ManagerListener;
 import com.techfly.liutaitai.util.Utility;
@@ -14,6 +16,7 @@ public class ServiceClick implements OnClickListener{
 	private Service mService;
 	private String mString;
 	private Context mContext;
+	private Dialog mDialog;
 
 	public ServiceClick(Context context, String string, Service service){
 		this.mContext = context;
@@ -24,11 +27,29 @@ public class ServiceClick implements OnClickListener{
 	@Override
 	public void onClick(View v) {
 		if(mContext.getString(R.string.order_service_btn).equals(mString)){
-			ManagerListener.newManagerListener().notifyServiceDeleteListener(mService);
+			mDialog = new Dialog(mContext, R.style.MyDialog);
+        	mDialog = AlertDialogUtils.displayMyAlertChoice(mContext, R.string.dialog_title, R.string.dialog_message, R.string.confirm, new View.OnClickListener() {
+                
+                @Override
+                public void onClick(View arg0) {
+                    mDialog.dismiss();
+                    ManagerListener.newManagerListener().notifyServiceDeleteListener(mService);
+                }
+            }, R.string.dialog_cancel, null);
+        	mDialog.show();
 		}else if(mContext.getString(R.string.order_service_btn1).equals(mString)){
 			ManagerListener.newManagerListener().notifyServicePayListener(mService);
 		}else if(mContext.getString(R.string.order_service_btn2).equals(mString)){
-			ManagerListener.newManagerListener().notifyServiceCancelListener(mService);
+			mDialog = new Dialog(mContext, R.style.MyDialog);
+        	mDialog = AlertDialogUtils.displayMyAlertChoice(mContext, R.string.dialog_title1, R.string.dialog_message2, R.string.confirm, new View.OnClickListener() {
+                
+                @Override
+                public void onClick(View arg0) {
+                    mDialog.dismiss();
+                    ManagerListener.newManagerListener().notifyServiceCancelListener(mService);
+                }
+            }, R.string.dialog_cancel, null);
+        	mDialog.show();
 		}else if(mContext.getString(R.string.order_service_btn3).equals(mString)){
 			Utility.call(mContext, Constant.KEFU_PHONE);
 		}else if(mContext.getString(R.string.order_service_btn4).equals(mString)){
--- MrsLiuClient/src/com/techfly/liutaitai/model/pcenter/adapter/OrderClick.java
+++ MrsLiuClient/src/com/techfly/liutaitai/model/pcenter/adapter/OrderClick.java
@@ -1,5 +1,6 @@
 package com.techfly.liutaitai.model.pcenter.adapter;
 
+import android.app.Dialog;
 import android.content.Context;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -7,6 +8,7 @@ import android.widget.Toast;
 
 import com.techfly.liutaitai.R;
 import com.techfly.liutaitai.model.pcenter.bean.TechOrder;
+import com.techfly.liutaitai.util.AlertDialogUtils;
 import com.techfly.liutaitai.util.Constant;
 import com.techfly.liutaitai.util.ManagerListener;
 import com.techfly.liutaitai.util.Utility;
@@ -16,6 +18,7 @@ public class OrderClick implements OnClickListener {
 	private TechOrder mOrder;
 	private String mString;
 	private int mIndex;
+	private Dialog mDialog;
 
 	public OrderClick(Context context, TechOrder order, String string, int index) {
 		this.mContext = context;
@@ -67,14 +70,22 @@ public class OrderClick implements OnClickListener {
 			}
 		} else if (mContext.getString(R.string.tech_order_list_btn5).equals(
 				mString)) {
-			if (mIndex == 0) {
-				ManagerListener.newManagerListener().notifyOrderDeleteListener(
-						mOrder);
-			} else if (mIndex == 1) {
-				ManagerListener.newManagerListener()
-						.notifyDetailOrderDeleteListener(mOrder);
-			}
-
+			mDialog = new Dialog(mContext, R.style.MyDialog);
+        	mDialog = AlertDialogUtils.displayMyAlertChoice(mContext, R.string.dialog_title, R.string.dialog_message1, R.string.confirm, new View.OnClickListener() {
+                
+                @Override
+                public void onClick(View arg0) {
+                    mDialog.dismiss();
+                    if (mIndex == 0) {
+        				ManagerListener.newManagerListener().notifyOrderDeleteListener(
+        						mOrder);
+        			} else if (mIndex == 1) {
+        				ManagerListener.newManagerListener()
+        						.notifyDetailOrderDeleteListener(mOrder);
+        			}
+                }
+            }, R.string.dialog_cancel, null);
+        	mDialog.show();
 		} else if (mContext.getString(R.string.tech_order_list_btn1).equals(
 				mString)) {
 			if (mIndex == 0) {
